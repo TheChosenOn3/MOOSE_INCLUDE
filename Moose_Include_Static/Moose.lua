ce529a00ad136f3435a1687960ba8692ac2feab0
FlightControl-User <sven.van.de.velde@telenet.be>
2018-06-18 15:57:02 +0000

    MOOSE include files (Moose.lua) [skip ci]

--- Moose_Include_Static/Moose.lua
+++ Moose_Include_Static/Moose.lua
@@ -1,4 +1,4 @@
-env.info( '*** MOOSE GITHUB Commit Hash ID: 2018-06-15T18:52:45.0000000Z-6fd937d93924916a6002b9184ea29e85b0e589b9 ***' )
+env.info( '*** MOOSE GITHUB Commit Hash ID: 2018-06-18T12:39:02.0000000Z-344c6b0973b9d7ecc1aaeaf4f7124a00c4e4b09c ***' )
 env.info( '*** MOOSE STATIC INCLUDE START *** ' )
 
 --- Various routines
